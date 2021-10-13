98d639184c69fd80be7c6c58d132196fe95933f9
Vitor Satyro <vitorsv1@al.insper.edu.br>
2021-10-13 10:01:36 -0300

    Traducao

--- scripts/hello.py
+++ scripts/hello.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python3
 import dev_aberto
+from datetime import *
+from gettext import gettext as _
+from babel.dates import format_date, format_datetime
+import gettext
 
-if __name__ == '__main__':
-    date, name = dev_aberto.hello()
-    print('Último commit feito em:', date, ' por', name)
+gettext.install("cli", localedir="locale")
+
+if __name__ == "__main__":
+    dat, name = dev_aberto.hello()
+    data = datetime.strptime(dat, "%Y-%m-%dT%H:%M:%S%z")
+    date2 = format_datetime(data, f"d/MMMM/Y  HH:mm:ss", locale=_("pt_BR.utf-8"))
+    print(_("Último commit feito em:"), date2, _(" por"), name)
